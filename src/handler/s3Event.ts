66d123e067efea709559faffebcc8ff5395936d6
ian-hodges <ian.hodges@bjss.com>
2022-06-13 10:17:44 +0100

    feat(cb2-4143): add basic s3 handler and unit test

--- src/handler/s3Event.ts
+++ src/handler/s3Event.ts
@@ -1,24 +1,25 @@
 import 'dotenv/config';
-import type { Context, ScheduledEvent } from 'aws-lambda';
+import type { Context, S3Event } from 'aws-lambda';
 import { createLogger, Logger } from '../util/logger';
 
 /**
  * Lambda Handler
  *
- * @param {ScheduledEvent} event
+ * @param {S3Event} event
  * @param {Context} context
  * @returns {Promise<Record<string, unknown>>}
  */
 export const handler = async (
-  event: ScheduledEvent,
+  event: S3Event,
   context: Context,
 ): Promise<Record<string, unknown>> => {
+  const bucketName = event.Records[0].s3.bucket.name;
   const logger: Logger = createLogger(null, context);
 
-  logger.info('Cloudwatch event successfully triggered!');
+  logger.info(`Triggered with ${bucketName}`);
 
   return Promise.resolve({
     statusCode: 200,
-    body: 'Cloudwatch event successfully triggered!',
+    body: `Triggered with ${bucketName}`,
   });
 };
--- tests/handler/s3Event.test.ts
+++ tests/handler/s3Event.test.ts
@@ -1,15 +1,16 @@
-import type { Context, ScheduledEvent } from 'aws-lambda';
+import type { Context, S3Event } from 'aws-lambda';
 import { v4 } from 'uuid';
-import { handler } from '../../src/handler/cloudWatchEvent';
+import { handler } from '../../src/handler/s3Event';
+import event from '../resources/s3event.json';
 
 describe('Test CloudWatch Event Lambda Function', () => {
   test('should return 200 with a success message', async () => {
-    const eventMock: ScheduledEvent = <ScheduledEvent>{};
+    const eventMock: S3Event = event as S3Event;
     const contextMock: Context = <Context>{ awsRequestId: v4() };
 
     const res: Record<string, unknown> = await handler(eventMock, contextMock);
 
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual('Cloudwatch event successfully triggered!');
+    expect(res.body).toEqual('Triggered with DOC-EXAMPLE-BUCKET');
   });
 });
