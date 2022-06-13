42cbfbebb0ce48c6938c521a49e1b4d22ab4d600
Nathan Brown <nathan.brown@bjss.com>
2022-06-13 16:31:33 +0100

    add test of header and footer

--- tests/fileConvert/fileConvert.test.ts
+++ tests/fileConvert/fileConvert.test.ts
@@ -1,18 +1,20 @@
 import { configureFile } from '../../src/fileConvert/fileConvert';
 import * as fs from 'fs';
+import * as zlib from 'zlib';
 import md5 from 'md5';
 
 describe('test the file config', () => {
 
   const formattedDate = new Date().toLocaleDateString('en-GB').split('/').reverse().join('');
   const txtFilename = 'crc32_' + formattedDate + '.txt';
-  const csvFilename = 'EVL_GVT_' + formattedDate + '.csv.gz';
+  const zipCsvFilename = 'EVL_GVT_' + formattedDate + '.csv.gz';
+  const csvFilename = 'EVL_GVT_' + formattedDate + '.csv';
   const finalFilename = 'EVL_GVT_' + formattedDate + '.tar.gz';
   const testFilename = 'tests/resources/EVL_GVT_TESTFILE.csv';
 
   afterEach(() => {
     fs.unlinkSync(finalFilename);
-    fs.unlinkSync(csvFilename);
+    fs.unlinkSync(zipCsvFilename);
     fs.unlinkSync(txtFilename);
   });
 
@@ -25,7 +27,7 @@ describe('test the file config', () => {
   test('expect csv filename to be correct', async () => {
     await configureFile(testFilename);
     const fileList = fs.readdirSync('./');
-    expect(fileList).toContain(csvFilename);
+    expect(fileList).toContain(zipCsvFilename);
   });
 
   test('expect txt filename to be correct', async () => {
@@ -34,9 +36,18 @@ describe('test the file config', () => {
     expect(fileList).toContain(txtFilename);
   });
 
+  test('expect csv to include the header and footer', async () => {
+    await configureFile(testFilename);
+    const zipCsvFile = fs.readFileSync(zipCsvFilename);
+    const csvFile = zlib.gunzipSync(zipCsvFile);
+    const csvData = csvFile.toString();
+    expect(csvData).toContain('HEADER LINE: EVL GVT TRANSFER FILE');
+    expect(csvData).toContain('TRAILER LINE: 2 RECORDS.');
+  });
+
   test('expect hash to match', async () => {
     await configureFile(testFilename);
-    const csvFile = fs.readFileSync(csvFilename);
+    const csvFile = fs.readFileSync(zipCsvFilename);
     const textFile = fs.readFileSync(txtFilename);
     const hash = md5(csvFile);
     expect(textFile.toString()).toBe(hash);
