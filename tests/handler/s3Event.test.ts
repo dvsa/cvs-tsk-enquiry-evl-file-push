2fbe58f1f8c523fed34f8ceb978cb47c2b1d84e7
ian-hodges <ian.hodges@bjss.com>
2022-06-14 14:31:25 +0100

    feat(cb2-4156): pull file from s3

--- tests/handler/s3Event.test.ts
+++ tests/handler/s3Event.test.ts
@@ -1,16 +1,14 @@
-import type { Context, S3Event } from 'aws-lambda';
-import { v4 } from 'uuid';
+import type { S3Event } from 'aws-lambda';
 import { handler } from '../../src/handler/s3Event';
 import event from '../resources/s3event.json';
 
 describe('Test S3 Event Lambda Function', () => {
   test('should return 200 with a success message', async () => {
     const eventMock: S3Event = event as S3Event;
-    const contextMock: Context = <Context>{ awsRequestId: v4() };
 
-    const res: Record<string, unknown> = await handler(eventMock, contextMock);
+    const res: Record<string, unknown> = await handler(eventMock);
 
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual('Triggered with DOC-EXAMPLE-BUCKET');
+    expect(res.body).toEqual('Triggered with s3-lambda-testing-7654');
   });
 });
