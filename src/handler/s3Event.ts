6de820d779aeb769ac4479125df00be6d8bdc9a7
Nathan Brown <nathan.brown@bjss.com>
2022-06-22 11:09:47 +0100

    feat(cb2-4347): Remove unused import

--- src/handler/s3Event.ts
+++ src/handler/s3Event.ts
@@ -5,8 +5,6 @@ import { configureFile } from '../fileConvert/fileConvert';
 import { filePull } from '../filePull/fromS3';
 import { filePush } from '../filePush/filePush';
 import { randomUUID } from 'crypto';
-import logger from '../util/logger';
-
 
 const handleEvent = async (record: S3EventRecord) => {
   const workingDir = `/tmp/${randomUUID()}/`;
