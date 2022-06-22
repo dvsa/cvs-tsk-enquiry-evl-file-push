584c79b8dc22776688bfa6ef2db8e5f50009f936
Nathan Brown <nathan.brown@bjss.com>
2022-06-22 11:06:28 +0100

    feat(cb2-4347): Create loop to process multiple S3 events and test

--- src/handler/s3Event.ts
+++ src/handler/s3Event.ts
@@ -1,10 +1,27 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 import * as fs from 'fs';
-import type { S3Event } from 'aws-lambda';
+import type { S3Event, S3EventRecord } from 'aws-lambda';
 import { configureFile } from '../fileConvert/fileConvert';
 import { filePull } from '../filePull/fromS3';
 import { filePush } from '../filePush/filePush';
 import { randomUUID } from 'crypto';
+import logger from '../util/logger';
+
+
+const handleEvent = async (record: S3EventRecord) => {
+  const workingDir = `/tmp/${randomUUID()}/`;
+  try {
+    fs.mkdirSync(workingDir);
+    const evlFileData = await filePull(record);
+    const filepath = await configureFile(workingDir, evlFileData.data, evlFileData.filename);
+    await filePush(filepath);
+  } catch (err) {
+    throw err;
+  } finally {
+    fs.rmSync(workingDir, { recursive: true, force: true });
+  }
+
+};
 
 /**
  * Lambda Handler
@@ -16,15 +33,16 @@ import { randomUUID } from 'crypto';
 export const handler = async (
   event: S3Event,
 ): Promise<Record<string, unknown>> => {
-  const workingDir = `/tmp/${randomUUID()}/`;
-  try {
-    fs.mkdirSync(workingDir);
-    const record = event.Records[0];
-    const evlFileData = await filePull(record);
-    const filepath = await configureFile(workingDir, evlFileData.data, evlFileData.filename);
-    await filePush(filepath);
-  } finally {
-    fs.rmSync(workingDir, { recursive: true, force: true });
+
+  for (const record of event.Records) {
+    try {
+      await handleEvent(record);
+    } catch  (err) {
+      console.error(err);
+      return Promise.resolve({
+        statusCode: 500,
+      });
+    }
   }
 
   return Promise.resolve({
