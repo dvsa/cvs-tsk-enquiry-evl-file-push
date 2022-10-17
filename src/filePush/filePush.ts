1f1915ac74c4efa7f39b0449140e3fd13fcd00ea
Nathan Brown <nathan.brown@bjss.com>
2022-10-17 13:06:22 +0100

    feat(cb2-4904): add some logging info

--- src/filePush/filePush.ts
+++ src/filePush/filePush.ts
@@ -32,6 +32,7 @@ export const createConfig = async (eventType: string) => {
 
 export const filePush = async (filepath: string, eventType: string) => {
   const config = await createConfig(eventType);
+  logger.info('Created config from secrets');
   const sftp = new Client();
   const sftpPath =
     eventType === 'evl' ? process.env.EVL_SFTP_PATH : process.env.TFL_SFTP_PATH;
@@ -39,6 +40,7 @@ export const filePush = async (filepath: string, eventType: string) => {
 
   try {
     await sftp.connect(config);
+    logger.info('Spliced data into the csv file');
     await sftp.fastPut(filepath, remoteFileLocation);
     logger.info('Successfully uploaded to SFTP');
   } catch (err) {
